5b3b08e7fda39ebfc46327258f517acfeec5f55f
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-06-27 11:33:28 +0700

    Change directory

--- router/routeUser.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router();
-const db = require("../base/db");
-const controller = require("../controller/controllerUser");
-
-Router.post("/users/add", controller.addUser);
-Router.get("/users/search", controller.getUserAll);
-
-
-module.exports = Router;
